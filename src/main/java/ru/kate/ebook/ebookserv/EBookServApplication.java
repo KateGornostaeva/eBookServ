affe98c579415930530f3cb1089dc8078ce0bceb
Kate <kate.gornostaeva@inbox.ru>
2024-11-15 12:36:49 +0300

    init commit

---
+++ src/main/java/ru/kate/ebook/ebookserv/EBookServApplication.java
@@ -0,0 +1,13 @@
+package ru.kate.ebook.ebookserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookServApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBookServApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/kate/ebook/ebookserv/EBookServApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kate.ebook.ebookserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBookServApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
